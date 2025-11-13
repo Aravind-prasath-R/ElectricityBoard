337964af10f9d8decdde1cc37d41c1310fb901af
AravindPrasath <aravindprasath296@gmail.com>
2025-11-13 14:29:10 +0530

    Fixed submodule issue and added backend/project files

---
+++ backend/project/app/urls.py
@@ -0,0 +1,19 @@
+
+from django.urls import path
+from . import views
+from . views import ConnectionListView
+
+
+urlpatterns = [
+    # path("", views.index,name="index"),
+    path("login/",views.login,name="login"),
+    path("uploaddata/",views.uploaddata,name="uploaddata"),
+    path("getApplicantsData/", ConnectionListView.as_view(), name="connection_list"),
+    path("update_applicant/<id>",views.update_applicant,name='update_applicant'),
+    path("connectionvisualization/",views.connectionvisualization,name="connectionvisualization"),
+    path("connectionrequestdata/",views.connectionrequestdata,name="connectionrequestdata"),
+    path('login/',views.handlelogin,name='handlelogin'), #login
+
+]
+
+# ""=="/"=="localhost"==!
